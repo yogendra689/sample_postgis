bb3d54031cc536d09d16def5d0e4f092427012a4
yogendra689 <yogendra.tala@kiprosh.com>
2015-02-20 20:57:24 +0530

    imp links

--- app/models/area.rb
+++ app/models/area.rb
@@ -9,3 +9,9 @@ class Area < ActiveRecord::Base
   	query = "select * from areas where st_contains(geometry, ST_GeomFromText('POINT(#{lat} #{lon})'))"
   end
 end
+
+
+# inserting polygon record
+# Area.new(name: "sdfasd", geometry: "POLYGON((-149.737965876574 61.1952881991104,
+#  -149.71848377896 61.1953198415937, -149.718483761252 61.1952938698801, 
+#  -149.718483872402 61.1951924591105, -149.737965876574 61.1952881991104))")
